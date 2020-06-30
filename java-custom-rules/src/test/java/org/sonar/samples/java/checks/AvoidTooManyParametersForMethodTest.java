2ab0ea8f5d17d26cf761cde020a23541dac5c4c5
kiran-k12 <62979107+kiran-k12@users.noreply.github.com>
2020-06-30 13:21:58 +0530

    Update AvoidTooManyParametersForMethodTest.java

--- java-custom-rules/src/test/java/org/sonar/samples/java/checks/AvoidTooManyParametersForMethodTest.java
+++ java-custom-rules/src/test/java/org/sonar/samples/java/checks/AvoidTooManyParametersForMethodTest.java
@@ -1,1 +1,13 @@
-tets
+package org.sonar.samples.java.checks;
+ 
+import org.junit.Test;
+import org.sonar.java.checks.verifier.JavaCheckVerifier;
+ 
+public class AvoidTooManyParametersForMethodTest {
+ 
+  @Test
+  public void verify() {
+   JavaCheckVerifier.verify("src/test/files/AvoidTooManyParametersForMethodCheck.java", new AvoidTooManyParametersForMethodRule());
+  }
+ 
+}
