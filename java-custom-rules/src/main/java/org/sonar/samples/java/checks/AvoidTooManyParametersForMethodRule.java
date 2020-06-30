985e065c43b38c42e1752754b2b029cdf51f4682
kiran-k12 <62979107+kiran-k12@users.noreply.github.com>
2020-06-30 12:43:57 +0530

    Create AvoidTooManyParametersForMethodRule.java

---
+++ java-custom-rules/src/main/java/org/sonar/samples/java/checks/AvoidTooManyParametersForMethodRule.java
@@ -0,0 +1,7 @@
+package org.sonar.samples.java.checks;
+
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+
+public class AvoidTooManyParametersForMethodRule extends IssuableSubscriptionVisitor{
+
+}
