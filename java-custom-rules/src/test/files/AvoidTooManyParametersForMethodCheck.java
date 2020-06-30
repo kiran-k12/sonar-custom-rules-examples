c2f3ddad5572827b945dc4f770d5d84495694204
kiran-k12 <62979107+kiran-k12@users.noreply.github.com>
2020-06-30 12:37:35 +0530

    Update AvoidTooManyParametersForMethodCheck.java

--- java-custom-rules/src/test/files/AvoidTooManyParametersForMethodCheck.java
+++ java-custom-rules/src/test/files/AvoidTooManyParametersForMethodCheck.java
@@ -1,9 +1,14 @@
 public class AvoidTooManyParametersForMethodCheck{
 
- public void methodTest(int input1, int input2, int input3, int input4, int input5, int input6){
- 
+ public void methodTest(int input1, int input2, int input3, int input4, int input5, int input6){  // Noncompliant {{Too Many Parameters}}
+    System.out.println(input1+input2+input3+input4+input5+input6);
  }
  
+  public void methodTest(int input1, int input2, int input3){  
+   
+    System.out.println(input1+input2+input3);
+ 
+ }
  
  }
  
